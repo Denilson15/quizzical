77389adcf157724a91bbd65cbdcfde0b25d93c2a
Denilson15 <galvarado1501@gmail.com>
2026-01-15 19:56:53 -0800

    adjusted overflow and width sizing for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import StartScreen from './Components/StartScreen'
